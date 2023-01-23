cb753430a82d736281814516254dc13f8176d758
Ran Fang <rf421@imperial.ac.uk>
2023-01-24 06:29:25 +0800

    1

--- poly/poly.py
+++ poly/poly.py
@@ -3,4 +3,18 @@
 class Poly:
     def __init__(self, coef):
         self.coefficient=coef
+    
+    def degree(self):
+        return len(self.coefficient) - 1
+    
+    def __str__(self):
+        coefs=self.coefficient
+        term=[]
+        if coefs[0]:
+            term.append(str(coefs[0]))
+        if self.degree()>0 and coefs[1]:
+            term.append(f"{coefs[1]}x")
+        
+        term += [f"{'' if c==1 else c}x^{d}"for d,c in enumerate(coefs[2:], start=2) if c]
 
+        return "+".join(reversed(term)) or "0"
